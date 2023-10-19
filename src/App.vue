f57762c6bfe16d6516bd4dda405eb19f97d8a5fa
bigtree <bigtree5064@gmail.com>
2023-10-20 07:59:53 +0800

    项目开始

---
+++ src/App.vue
@@ -0,0 +1,134 @@
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+import { Document, Menu as IconMenu, Location, Setting } from '@element-plus/icons-vue'
+const handleOpen = (key: string, keyPath: string[]) => {
+  console.log(key, keyPath)
+}
+const handleClose = (key: string, keyPath: string[]) => {
+  console.log(key, keyPath)
+}
+
+const activeIndex = ref('1')
+const handleSelect = (key: string, keyPath: string[]) => {
+  console.log(key, keyPath)
+}
+</script>
+<template>
+  <div class="common-layout">
+    <!-- 容器 -->
+    <el-container>
+      <!-- 头部 -->
+      <el-header>
+        <el-menu
+          :default-active="activeIndex"
+          class="el-menu-demo"
+          mode="horizontal"
+          :ellipsis="false"
+          @select="handleSelect"
+        >
+          <el-menu-item index="0">
+            <img style="width: 40px" src="/images/bigtree.png" alt="bigtree" />
+          </el-menu-item>
+          <div class="flex-grow" />
+          <el-menu-item index="1">Processing Center</el-menu-item>
+          <el-sub-menu index="2">
+            <template #title>Workspace</template>
+            <el-menu-item index="2-1">item one</el-menu-item>
+            <el-menu-item index="2-2">item two</el-menu-item>
+            <el-menu-item index="2-3">item three</el-menu-item>
+            <el-sub-menu index="2-4">
+              <template #title>item four</template>
+              <el-menu-item index="2-4-1">item one</el-menu-item>
+              <el-menu-item index="2-4-2">item two</el-menu-item>
+              <el-menu-item index="2-4-3">item three</el-menu-item>
+            </el-sub-menu>
+          </el-sub-menu>
+        </el-menu>
+      </el-header>
+      <!-- 内容容器 -->
+      <el-container>
+        <!-- 导航栏 -->
+        <el-aside width="200px">
+          <el-row class="tac">
+            <el-col :span="24">
+              <el-menu
+                default-active="2"
+                class="el-menu-vertical-demo"
+                @open="handleOpen"
+                @close="handleClose"
+              >
+                <el-sub-menu index="1">
+                  <template #title>
+                    <el-icon><location /></el-icon>
+                    <span>Navigator One</span>
+                  </template>
+                  <el-menu-item-group title="Group One">
+                    <el-menu-item index="1-1">item one</el-menu-item>
+                    <el-menu-item index="1-2">item two</el-menu-item>
+                  </el-menu-item-group>
+                  <el-menu-item-group title="Group Two">
+                    <el-menu-item index="1-3">item three</el-menu-item>
+                  </el-menu-item-group>
+                  <el-sub-menu index="1-4">
+                    <template #title>item four</template>
+                    <el-menu-item index="1-4-1">item one</el-menu-item>
+                  </el-sub-menu>
+                </el-sub-menu>
+                <el-menu-item index="2">
+                  <el-icon><icon-menu /></el-icon>
+                  <span>Navigator Two</span>
+                </el-menu-item>
+                <el-menu-item index="3" disabled>
+                  <el-icon><document /></el-icon>
+                  <span>Navigator Three</span>
+                </el-menu-item>
+                <el-menu-item index="4">
+                  <el-icon><setting /></el-icon>
+                  <span>Navigator Four</span>
+                </el-menu-item>
+              </el-menu>
+            </el-col>
+          </el-row>
+        </el-aside>
+        <!-- 主要内容 -->
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+
+
+<style lang="less">
+.common-layout {
+  width: 100%;
+  height: 100%;
+  // height: 10000px;
+  box-sizing: border-box;
+  .el-header {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    // padding: 15px;
+    & > .el-menu {
+      position: sticky;
+      top: 0;
+      left: 0;
+      // width: 100%;
+      padding: 15px;
+    }
+  }
+  .el-aside {
+    position: fixed;
+    top: 60px;
+    left: 0;
+    height: 100%;
+    // background-color: #666;
+  }
+}
+
+.flex-grow {
+  flex-grow: 1;
+}
+</style>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <button>按钮</button>
+    <el-button type="primary">Primary</el-button>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
